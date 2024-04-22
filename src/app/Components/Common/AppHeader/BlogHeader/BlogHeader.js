28aad76ab805fd6b99745f80890aadb2056af8f5
nyangweso-rodgers <rodgerso65@gmail.com>
2024-04-22 22:42:46 +0300

    created folders for various components templates

---
+++ src/app/Components/Common/AppHeader/BlogHeader/BlogHeader.js
@@ -0,0 +1,16 @@
+const BlogHeader = () => {
+  return (
+    <>
+      <header className={`border p-5`}>
+        <nav className={`navbar navbar-expand-md`}>
+          <div className={`container`}>
+            <a className={`navbar-brand`} href="#">
+              Blog Header
+            </a>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+};
+export default BlogHeader;
