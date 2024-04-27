20cd124d89ddb6adc4b9227e86dd6ab20739a100
nyangweso-rodgers <rodgerso65@gmail.com>
2024-04-27 11:32:41 +0300

    added surveyheader component

---
+++ src/app/Components/Common/AppHeader/SurveyHeader/SurveyHeader.js
@@ -0,0 +1,17 @@
+//SurveyHeader component
+
+const SurveyHeader = () => {
+  return (
+    <>
+      <header>
+        <nav className={`navbar navbar-expand-md`}>
+          <div className={`container`}>
+            <span className={`navbar-brand`}>Survey App Header</span>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default SurveyHeader;
