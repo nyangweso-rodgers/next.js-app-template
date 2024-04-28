ae15a337b1c3802fa1a9b403cdc067fc1bb8d74b
nyangweso-rodgers <rodgerso65@gmail.com>
2024-04-28 12:40:28 +0300

    added MeetOurTeam section component

---
+++ src/app/Components/ImagesTemplates/OurTeamSection/OurTeamSection.js
@@ -0,0 +1,53 @@
+"use client";
+import styles from "../../../page.module.css";
+import Image from "next/image";
+const OurTeamSection = () => {
+  return (
+    <>
+      <main>
+        <div className={`container border border-primary`}>
+          <div className={`row border`}>
+            <div className={`col-md-4`}>
+              <a href="#" className={`card`}>
+                {/*<Image
+                  src="/images/google-bigquery-logo.png"
+                  alt="Google BigQuery Logo"
+                  width="800"
+                  height="600"
+                  layout="responsive"
+                />*/}
+                <div className={`card-body`}></div>
+              </a>
+            </div>
+            <div className={`col-md-4`}>
+              <a href="#" className={`card`}>
+                {/*<Image
+                  src="/images/JavaScript-logo.png"
+                  alt="Google BigQuery Logo"
+                  width="800"
+                  height="600"
+                  layout="responsive"
+                />*/}
+                <div className={`card-body`}></div>
+              </a>
+            </div>
+            <div className={`col-md-4`}>
+              <a href="#" className={`card`}>
+                {/*<Image
+                  src="/images/next.js-logo.jpg"
+                  alt="Google BigQuery Logo"
+                  width="800"
+                  height="600"
+                  layout="responsive"
+                />*/}
+                <div className={`card-body`}></div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default OurTeamSection;
