fd59d15861909e54d4e8e39a71c42ce28661f804
nyangweso-rodgers <rodgerso65@gmail.com>
2024-05-03 19:50:59 +0300

    added app portal template

---
+++ src/app/components/PortalTemplates/AppPortal.js
@@ -0,0 +1,26 @@
+//App Portal
+import styles from "../../page.module.css";
+const AppPortal = () => {
+  return (
+    <>
+      <main>
+        <div className="container">
+          <div className={`row`}>
+            <div className={`col-md-4`}>
+              <div></div>
+            </div>
+            <div className={`col-md-4`}></div>
+            <div className={`col-md-4`}></div>
+          </div>
+          <div className={`row`}>
+            <div className={`col-md-4`}></div>
+            <div className={`col-md-4`}></div>
+            <div className={`col-md-4`}></div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default AppPortal;
