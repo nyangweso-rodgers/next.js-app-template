c555f3cb21b60befe06a629e18054a4372d8508d
nyangweso-rodgers <rodgerso65@gmail.com>
2024-05-08 21:26:49 +0300

    added 3 header templates

---
+++ src/app/components/header-templates/blog-header-templates/page.js
@@ -0,0 +1,18 @@
+"use client";
+
+const BlogPage = () => {
+  return (
+    <>
+      <header>
+        <nav className={`navbar navbar-expand-md`}>
+          <div className={`container`}>
+            <a className={`navbar-brand`} href="#">
+              Blog Header
+            </a>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+};
+export default BlogPage;
---
+++ src/app/components/header-templates/survey-app-templates/page.js
@@ -0,0 +1,16 @@
+
+const SurveyHeaderPage = () => {
+  return (
+    <>
+      <header>
+        <nav className={`navbar navbar-expand-md`}>
+          <div className={`container`}>
+            <span className={`navbar-brand`}>Survey App Header</span>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default SurveyHeaderPage;
