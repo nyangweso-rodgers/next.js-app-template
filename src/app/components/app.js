48e0f0ce13db58eaa6746ecbb986d5ecbe6565e6
Rodgers Nyangweso <rodgerso65@gmail.com>
2024-10-05 21:42:23 +0300

    update package.json and layout

--- src/app/components/app.js
+++ src/app/components/app.js
@@ -3,15 +3,22 @@ import ShowVisitorsFormPage from "../components/form-templates/show-visitors-sur
 import ExhibitionSurveyFormPage from "../components/form-templates/exhibitors-survey-form/page.js";
 import FooterPage from "../components/footer-templates/01-sample-footer/page.js";
 
+import CardTemplateOne from "../components/card-templates/01-cards/page.js";
+import ProfileCardTemplate from "./card-templates/02-profile-card-template/page.js";
+import BlogPostCardTemplate from "./card-templates/03-blog-post-card-template/page.js";
+
 const App = () => {
   return (
     <>
-      <ShowVisitorsFormPage />
+      {/*<ShowVisitorsFormPage />*/}
       {/*<ExhibitionSurveyFormPage />*/}
       {/*<SurveyAppHeader />
       <AppFooter />*/}
 
-      <FooterPage />
+      {/*<FooterPage />*/}
+      <CardTemplateOne />
+      <ProfileCardTemplate />
+      <BlogPostCardTemplate />
     </>
   );
 };
