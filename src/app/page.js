db78e54faae07b193c1c31d49f498cbe7618684d
Rodgers Nyangweso <rodgerso65@gmail.com>
2024-10-06 21:57:08 +0300

    reference cards components

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,14 @@ import "bootstrap/dist/css/bootstrap.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./styles/globals.css";
 
-import App from './components/app';
+import ProfileCardTemplate from "./components/03-card-templates/02-profile-card-template/page.js";
+import BlogPostCardTemplate from "./components/03-card-templates/03-blog-post-card-template/page.js";
 
 export default function Home() {
-  return <App />;
+  return (
+    <>
+      <ProfileCardTemplate />
+      <BlogPostCardTemplate />
+    </>
+  );
 }
